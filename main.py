886cd0d216ea064090434218e61d712de142faba
harishvraghav <harishvraghav@gmail.com>
2014-09-07 13:00:46 +0530

    commit 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from PyQt4 import QtGui, QtCore
 
 from DigitalStampAlbum.MainWindow import MainWindow
